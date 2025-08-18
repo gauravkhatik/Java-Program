0cf7771d6ef0f313255a0a2a0a0be762549d7ed5
gauravkhatik <gauravkkhatik07@gmail.com>
2025-08-18 11:31:47 +0530

    New Open Again

---
+++ Array/simple.java
@@ -0,0 +1,15 @@
+package Array;
+
+public class simple {
+  public static void main(String[] args) {
+    int[] myarr = { 2, 1, 4, 1, 3, 22 };
+    System.out.println(myarr[4]);
+    System.out.println(myarr);
+    int i = 0;
+    while(i <= myarr.length){
+      System.out.println(myarr[i]);
+    }
+
+  }
+
+}
