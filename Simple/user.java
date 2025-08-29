ad57814a5bce74dd64dd3607f7840333595e6efe
gauravkhatik <gauravkkhatik07@gmail.com>
2025-08-29 21:44:30 +0530

    input

---
+++ Simple/user.java
@@ -0,0 +1,14 @@
+package Simple;
+
+import java.util.Scanner;
+
+public class user {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter Your Name: ");
+    String name = input.nextLine();
+    System.out.println("Nmae :- "+name);
+
+
+  }
+}
